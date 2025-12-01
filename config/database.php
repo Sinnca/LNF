f11dc76b118ab742543289fea3ee156e58e5e92e
Sinnca <vaniverana@gmail.com>
2025-12-01 18:58:31 +0800

    changes

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'l&f');
+define('DB_NAME', 'lnf');
 
 // Attempt to connect to MySQL database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
