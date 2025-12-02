03760bd91f69adf9c94a5c8f6a2b1c07f3a7d1cd
Kenneth Ace Tolentino <kdkennethtolentino@gmail.com>
2025-12-02 18:07:10 +0800

    Improve login backend: add institutional email check, email existence validation, and secure session handling

--- config/database.php
+++ config/database.php
@@ -5,6 +5,7 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'lnfdb');
 
+
 // Attempt to connect to MySQL database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
